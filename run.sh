f5d065b274dc50599ae3d37d1aa990edc2a4a21c
mordechai <mordechai.hadad01@gmail.com>
2021-10-01 18:03:54 +0300

    very nice

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 WD=$PWD # Save conf location
-RUNZSH=no
+export RUNZSH=no
+OS=$(uname -r)
 
 # Download and Install yay
 cd /tmp
@@ -16,24 +17,17 @@ cd ..
 rm -rf yay
 echo "removed yay directory"
 
-sudo pacman-key --init
-sudo pacman-key --populate archlinux
+echo "Successfully installed yay"
+
+if echo $OS | grep "WSL" > /dev/null; then
+    # Initing and populating pacman key
+    sudo pacman-key --init
+    sudo pacman-key --populate archlinux
+fi
 
 # Install packages
+echo "Installing good packages"
 yay -S neovim zsh neofetch wget nodejs lazygit lsd clang
-
-
-# Configure internet
-sudo systemctl enable NetworkManager
-echo "enabled NetworkManager.service"
-
-mkdir -p $HOME/.config/neofetch
-
-# Configure nvim
-cd $HOME/.config/
-echo "moved to $PWD"
-ln -sf $WD/nvim nvim
-echo "linking $WD/nvim to $PWD/nvim"
 
 # Download and install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -45,15 +39,27 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# symbol link zsh related files
+# Configure nvim
+ln -sf $WD/nvim $HOME/.config/nvim
+echo "linking $WD/nvim to $HOME/.config/nvim"
+
+# Configure zsh
 ln -sf $WD/.zshrc $HOME/.zshrc
 echo "linking $WD/.zshrc to $HOME/.zshrc"
 
 ln -sf $WD/.p10k.zsh $HOME/.p10k.zsh
 echo "linking $WD/.p10k.zsh to $HOME/.p10k.zsh"
 
-ln -sf $WD/neofetch/config.conf $HOME/.config/neofetch/config.conf
-echo "linking $WD/neofetch/config.conf to $HOME/.config/neofetch/config.conf"
+# Configure neofetch
+ln -sf $WD/neofetch $HOME/.config/neofetch
+echo "linking $WD/neofetch to $HOME/.config/neofetch"
 
-# start zsh
+# Check if using WSL
+if ! echo $OS | grep "WSL" > /dev/null; then
+    echo "Detected not using WSL setting up GUI dotfiles"
+    sudo systemctl enable NetworkManager
+    echo "enabled NetworkManager.service"
+fi
+
+# Start zsh
 exec zsh -l
