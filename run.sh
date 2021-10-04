8731b9d18a90f860c0cc7bbf20a3ba824525b62f
mordechai <mordechai.hadad01@gmail.com>
2021-10-04 19:41:31 +0300

    epic

--- run.sh
+++ run.sh
@@ -39,6 +39,7 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 # Download epic zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
 
 # Configure nvim
 ln -sf $WD/nvim $HOME/.config/nvim
