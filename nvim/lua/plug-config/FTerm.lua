3192f2d1491a1e89afdfa387cc7a6dc34b73efd3
mordechai <mordechai.hadad01@gmail.com>
2021-10-17 18:37:13 +0300

    wtfsda

--- nvim/lua/plug-config/FTerm.lua
+++ nvim/lua/plug-config/FTerm.lua
@@ -1,7 +1,7 @@
 return function()
-	local keymap = vim.api.nvim_set_keymap
-	local opts = { noremap = true, silent = true }
+    local keymap = vim.api.nvim_set_keymap
+    local opts = { noremap = true, silent = true }
 
-	-- keymap("n", "<F12>", '<CMD>lua require("FTerm").toggle()<CR>', opts)
-	-- keymap("t", "<F12>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', opts)
+    -- keymap("n", "<F12>", '<CMD>lua require("FTerm").toggle()<CR>', opts)
+    -- keymap("t", "<F12>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', opts)
 end
