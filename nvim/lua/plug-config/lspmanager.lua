b750c71ccdfddb96c276ff011b620ad916fbc143
mordechai <mordechai.hadad01@gmail.com>
2021-10-17 18:36:43 +0300

    asdas

--- nvim/lua/plug-config/lspmanager.lua
+++ nvim/lua/plug-config/lspmanager.lua
@@ -1,12 +1,12 @@
 return function()
-    local luadev = require("lua-dev").setup({
-        library = {
-            vimruntime = true,
-            types = true,
-            plugins = false,
-        },
-    })
-    require("lspmanager").setup({
-        sumneko_lua = luadev,
-    })
+	local luadev = require("lua-dev").setup({
+		library = {
+			vimruntime = true,
+			types = true,
+			plugins = false,
+		},
+	})
+	require("lspmanager").setup({
+		sumneko_lua = luadev,
+	})
 end
