6b6b347b130b5cf147ac2129ad478fc9f50cbf0d
mordechai <mordechai.hadad01@gmail.com>
2021-10-18 09:31:40 +0300

    man wtf

--- nvim/lua/plug-config/package-info.lua
+++
@@ -1,6 +0,0 @@
-require("package-info").setup({
-    colors = {
-        up_to_date = "#98C379", -- Text color for up to date package virtual text
-        outdated = "#d94848", -- Text color for outdated package virtual text
-    },
-})
