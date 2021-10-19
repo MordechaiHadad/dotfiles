198f85c597bdcf9db0c9a7f1a6a52f6934a2af2d
mordechai <mordechai.hadad01@gmail.com>
2021-10-19 18:32:51 +0300

    oops

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -31,6 +31,6 @@ vim.o.showtabline = 2
 -- Completion
 vim.cmd("set shortmess+=c")
 
-if vim.loop..os_uname().sysname == "Windows_NT" then
+if vim.loop.os_uname().sysname == "Windows_NT" then
     vim.g.sqlite_clib_path = "C:/Program Files/Sqlite/sqlite3.dll"
 end
