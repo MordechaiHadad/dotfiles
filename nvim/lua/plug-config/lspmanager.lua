4b9384c0a89840f8b49964d7ac115f268e81a895
mordechai <mordechai.hadad01@gmail.com>
2021-11-05 10:59:18 +0200

    illuminate

--- nvim/lua/plug-config/lspmanager.lua
+++ nvim/lua/plug-config/lspmanager.lua
@@ -5,11 +5,21 @@ return function()
             types = true,
             plugins = false,
         },
+        lspconfig = {
+            on_attach = function (client)
+                require("illuminate").on_attach(client)
+            end,
+        }
     })
 
     require("lspmanager").setup({
         lsps = {
             sumneko_lua = luadev,
+            rust_analyzer = {
+                on_attach = function (client)
+                    require("illuminate").on_attach(client)
+                end,
+            }
         },
     })
 end
