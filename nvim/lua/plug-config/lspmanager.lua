867a0ebeefb82537e43af8f2646850a1675506b4
MordechaiHadad <mordechai.hadad01@gmail.com>
2021-12-06 19:38:10 +0200

    asdad

--- nvim/lua/plug-config/lspmanager.lua
+++ nvim/lua/plug-config/lspmanager.lua
@@ -1,34 +1,34 @@
 return function()
-	local luadev = require("lua-dev").setup({
-		library = {
-			vimruntime = true,
-			types = true,
-			plugins = false,
-		},
-		lspconfig = {
-			on_attach = function(client)
-				require("illuminate").on_attach(client)
-			end,
-		},
-	})
+    local luadev = require("lua-dev").setup({
+        library = {
+            vimruntime = true,
+            types = true,
+            plugins = false,
+        },
+        lspconfig = {
+            on_attach = function(client)
+                require("illuminate").on_attach(client)
+            end,
+        },
+    })
 
-	require("lspmanager").setup({
-		lsps = {
-			sumneko_lua = luadev,
-			rust_analyzer = {
-				on_attach = function(client)
-					require("illuminate").on_attach(client)
-				end,
-			},
-			pyright = {
-				on_attach = function(client)
-					require("illuminate").on_attach(client)
-				end,
-			},
-		},
-		ensure_installed = {
-			"sumneko_lua",
-			"rust_analyzer",
-		},
-	})
+    require("lspmanager").setup({
+        lsps = {
+            sumneko_lua = luadev,
+            rust_analyzer = {
+                on_attach = function(client)
+                    require("illuminate").on_attach(client)
+                end,
+            },
+            pyright = {
+                on_attach = function(client)
+                    require("illuminate").on_attach(client)
+                end,
+            },
+        },
+        ensure_installed = {
+            "sumneko_lua",
+            "rust_analyzer",
+        },
+    })
 end
