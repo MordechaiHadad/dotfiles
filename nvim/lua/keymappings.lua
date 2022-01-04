e31057d4f586e3c6bce92a63de8201d3a13e488c
MordechaiHadad <mordechai.hadad01@gmail.com>
2022-01-04 19:59:01 +0200

    Added based plugin for moving lines ZAMN!

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -10,15 +10,16 @@ keymap("i", "<C-s>", "<C-o>:w<CR>", opts) -- Save file in insert mode
 keymap("n", "tt", ":TSPlaygroundToggle<CR>", opts) -- Toggle TreeSitter playground
 
 -- Move selected line / block of text in visual mode
-keymap("x", "<C-Up>", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<C-Down>", ":move '>+1<CR>gv-gv", opts)
+keymap( "x", "<C-Up>", "<Plug>GoVSMUp", {} )
+keymap( "x", "<C-Down>", "<Plug>GoVSMDown", {} )
 
 -- Move line in normal mode
-keymap("n", "<C-Up>", ":m .-2<CR>", opts)
-keymap("n", "<C-Down>", ":m .+1<CR>", opts)
+keymap("n", "<C-Up>", "<Plug>GoNSMUp", {})
+keymap("n", "<C-Down>", "<Plug>GoNSMDown", {})
 -- Move line in insert mode
-keymap("i", "<C-Up>", "<C-o>:m .-2<CR>", opts)
-keymap("i", "<C-Down>", "<C-o>:m .+1<CR>", opts)
+keymap("i", "<C-Up>", "<C-o><Plug>GoNSMUp", {})
+keymap("i", "<C-Down>", "<C-o><Plug>GoNSMDown", {})
+
 
 -- Telescope keymaps
 keymap("n", "ff", ":Telescope find_files<CR>", opts) -- Find files
