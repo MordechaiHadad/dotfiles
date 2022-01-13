9472e1063dcc015b7392bce62d8ebb171625c631
MordechaiHadad <mordechai.hadad01@gmail.com>
2022-01-13 17:19:49 +0200

    sadasd

---
+++ nvim/lua/plug-config/mappy.lua
@@ -0,0 +1,7 @@
+return function ()
+    require("mappy")({
+        version = "stable"
+    })
+
+    local module = require("mappy"):new()
+end
--- nvim/lua/plug-config/themer.lua
+++ nvim/lua/plug-config/themer.lua
@@ -1,6 +1,5 @@
 return function()
     require("themer").setup({
         colorscheme = "papa_dark",
-        dim_inactive = true,
     })
 end
