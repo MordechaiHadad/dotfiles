db5bacbc2dec5c229be5015b35a63377a6bd9773
MordechaiHadad <mordechai.hadad01@gmail.com>
2022-05-12 11:44:11 +0300

    fix this shit

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -26,16 +26,16 @@ return packer.startup(function(use)
     use("wbthomason/packer.nvim")
 
     -- Colorscheme
-    use({ "/home/morde/repos/themer.lua", config = require("configs.themer") })
-    use({ "nvim-treesitter/nvim-treesitter", config = require("configs.treesitter"), run = ":TSUpdate" })
+    use({ "/home/morde/repos/themer.lua", config = require("plugins.configs.themer") })
+    use({ "nvim-treesitter/nvim-treesitter", config = require("plugins.configs.treesitter"), run = ":TSUpdate" })
     use({ "nvim-treesitter/playground", cmd = { "TSPlaygroundToggle", "TSHighlightCapturesUnderCursor" } })
-    use({ "RRethy/vim-illuminate", config = require("configs.illuminate"), event = "BufWinEnter" })
+    use({ "RRethy/vim-illuminate", config = require("plugins.configs.illuminate"), event = "BufWinEnter" })
 
     -- UI
-    use({ "romgrk/barbar.nvim", config = require("configs.barbar") })
-    use({ "nvim-lualine/lualine.nvim", config = require("configs.lualine") })
-    use({ "kyazdani42/nvim-tree.lua", config = require("configs.nvim-tree"), cmd = "NvimTreeToggle" })
-    use({ "startup-nvim/startup.nvim", config = require("configs.startup") })
+    use({ "romgrk/barbar.nvim", config = require("plugins.configs.barbar") })
+    use({ "nvim-lualine/lualine.nvim", config = require("plugins.configs.lualine") })
+    use({ "kyazdani42/nvim-tree.lua", config = require("plugins.configs.nvim-tree"), cmd = "NvimTreeToggle" })
+    use({ "startup-nvim/startup.nvim", config = require("plugins.configs.startup") })
     use("kyazdani42/nvim-web-devicons")
     use({
         "luukvbaal/stabilize.nvim",
@@ -44,15 +44,15 @@ return packer.startup(function(use)
         end,
         event = "BufWinEnter",
     })
-    use({ "folke/trouble.nvim", config = require("configs.lsp-trouble"), event = "BufWinEnter" })
+    use({ "folke/trouble.nvim", config = require("plugins.configs.lsp-trouble"), event = "BufWinEnter" })
 
     -- Autocomplete
-    use({ "hrsh7th/nvim-cmp", config = require("configs.cmp"), event = "BufWinEnter" })
+    use({ "hrsh7th/nvim-cmp", config = require("plugins.configs.cmp"), event = "BufWinEnter" })
     use({ "hrsh7th/cmp-buffer", after = "nvim-cmp" })
     use({ "hrsh7th/cmp-nvim-lsp", after = "nvim-cmp" })
     use({ "hrsh7th/cmp-path", after = "nvim-cmp" })
     use({"rafamadriz/friendly-snippets", after ="nvim-cmp"})
-    use({ "L3MON4D3/LuaSnip", config = require("configs.luasnip"), after = "nvim-cmp" })
+    use({ "L3MON4D3/LuaSnip", config = require("plugins.configs.luasnip"), after = "nvim-cmp" })
     use({ "saadparwaiz1/cmp_luasnip", after = "nvim-cmp" })
     use({ "hrsh7th/cmp-calc", after = "nvim-cmp" })
     use({ "hrsh7th/cmp-cmdline", after = "nvim-cmp" })
@@ -61,8 +61,8 @@ return packer.startup(function(use)
 
     -- LSP
     use("neovim/nvim-lspconfig")
-    use({ "jose-elias-alvarez/null-ls.nvim", config = require("configs.null-ls") })
-    use({ "williamboman/nvim-lsp-installer", config = require("configs.nvim-lsp-installer") })
+    use({ "jose-elias-alvarez/null-ls.nvim", config = require("plugins.configs.null-ls") })
+    use({ "williamboman/nvim-lsp-installer", config = require("plugins.configs.nvim-lsp-installer") })
     use({
         "simrat39/rust-tools.nvim",
         config = function()
@@ -83,14 +83,14 @@ return packer.startup(function(use)
             },
         },
         cmd = "Telescope",
-        config = require("configs.telescope"),
+        config = require("plugins.configs.telescope"),
     })
     use({
         "AckslD/nvim-neoclip.lua",
         requires = {
             { "tami5/sqlite.lua", module = "sqlite" },
         },
-        config = require("configs.neoclip"),
+        config = require("plugins.configs.neoclip"),
         after = "telescope.nvim",
     })
 
@@ -102,31 +102,31 @@ return packer.startup(function(use)
             "nvim-lua/plenary.nvim",
             "sindrets/diffview.nvim",
         },
-        config = require("configs.neogit"),
+        config = require("plugins.configs.neogit"),
     })
     use({
         "lewis6991/gitsigns.nvim",
         requires = "nvim-lua/plenary.nvim",
-        config = require("configs.gitsigns"),
+        config = require("plugins.configs.gitsigns"),
         event = "BufWinEnter",
     })
 
     -- Lang Dev
-    use({ "p00f/godbolt.nvim", run = "rm -rf fnl/", event = "BufWinEnter", config = require("configs.godbolt") })
+    use({ "p00f/godbolt.nvim", run = "rm -rf fnl/", event = "BufWinEnter", config = require("plugins.configs.godbolt") })
 
     -- Web Dev
     use({ "windwp/nvim-ts-autotag", ft = { "html", "svelte" } })
 
     -- Note Taking
-    use({ "nvim-neorg/neorg", config = require("configs.neorg"), ft = "norg" })
+    use({ "nvim-neorg/neorg", config = require("plugins.configs.neorg"), ft = "norg" })
 
     -- Editing Enhancments
-    use({ "windwp/nvim-autopairs", event = "InsertEnter", config = require("configs.autopairs") })
+    use({ "windwp/nvim-autopairs", event = "InsertEnter", config = require("plugins.configs.autopairs") })
     use({
         "folke/todo-comments.nvim",
         event = "BufWinEnter",
         requires = "nvim-lua/plenary.nvim",
-        config = require("configs.todo-comments"),
+        config = require("plugins.configs.todo-comments"),
     })
     use({
         "numToStr/Comment.nvim",
@@ -145,15 +145,15 @@ return packer.startup(function(use)
     --         require("goto-preview").setup({})
     --     end,
     -- })
-    use({ "akinsho/toggleterm.nvim", config = require("configs.toggleterm"), keys = "<F12>" })
-    use({ "folke/which-key.nvim", config = require("configs.which-key"), event = "BufWinEnter" })
+    use({ "akinsho/toggleterm.nvim", config = require("plugins.configs.toggleterm"), keys = "<F12>" })
+    use({ "folke/which-key.nvim", config = require("plugins.configs.which-key"), event = "BufWinEnter" })
     use({
         "folke/persistence.nvim",
         event = "BufReadPre",
         module = "persistence",
-        config = require("configs.persistence"),
+        config = require("plugins.configs.persistence"),
     })
-    use({ "ahmedkhalf/project.nvim", config = require("configs.project"), event = "BufWinEnter" })
+    use({ "ahmedkhalf/project.nvim", config = require("plugins.configs.project"), event = "BufWinEnter" })
     use({
         "michaelb/sniprun",
         run = "bash ./install.sh",
