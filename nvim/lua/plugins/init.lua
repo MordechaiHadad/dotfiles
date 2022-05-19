748be78478d29a2f2900f8c9f93b537476539478
MordechaiHadad <mordechai.hadad01@gmail.com>
2022-05-19 20:52:22 +0300

    bye barbar, hello bufferline

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -32,7 +32,7 @@ return packer.startup(function(use)
     use({ "RRethy/vim-illuminate", config = require("plugins.configs.illuminate"), event = "BufWinEnter" })
 
     -- UI
-    use({ "romgrk/barbar.nvim", config = require("plugins.configs.barbar") })
+    use({ "akinsho/bufferline.nvim", config = require("plugins.configs.bufferline") })
     use({ "nvim-lualine/lualine.nvim", config = require("plugins.configs.lualine") })
     use({ "kyazdani42/nvim-tree.lua", config = require("plugins.configs.nvim-tree"), cmd = "NvimTreeToggle" })
     use({ "startup-nvim/startup.nvim", config = require("plugins.configs.startup") })
