c57b771898433be2786dc502497f97fe93e1efef
MordechaiHadad <mordechai.hadad01@gmail.com>
2026-03-08 19:40:33 +0200

    Added: Heirline

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,25 +1,30 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = {},
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "neovim/nvim-lspconfig",
+    {
+        "mason-org/mason.nvim",
+        lazy = false,
+        opts = {},
     },
-    opts = {
-      ensure_installed = {
-        -- install non-Rust servers here
-        "svelte",
-        "lua_ls",
-      },
-      -- IMPORTANT: don't auto-setup rust_analyzer if using rustaceanvim
-      -- (prevents conflicts / double setup)
-      handlers = {
-        ["rust_analyzer"] = function() end,
-      },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        lazy = false,
+        dependencies = {
+            "mason-org/mason.nvim",
+            "neovim/nvim-lspconfig",
+        },
+        opts = {
+            ensure_installed = {
+                -- install non-Rust servers here
+                "svelte",
+                "lua_ls",
+            },
+            automatic_enable = {
+                exclude = { "rust_analyzer" }
+            },
+            -- IMPORTANT: don't auto-setup rust_analyzer if using rustaceanvim
+            -- (prevents conflicts / double setup)
+            handlers = {
+                ["rust_analyzer"] = function() end,
+            },
+        },
     },
-  },
 }
