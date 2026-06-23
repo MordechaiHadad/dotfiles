dad8bff02ba715ce9c4ea114708c6e394f9a7f49
MordechaiHadad <mordechai.hadad01@gmail.com>
2026-06-23 14:50:53 +0300

    updated blink

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -26,15 +26,16 @@ return {
 	},
 	{
 		"saghen/blink.pairs",
-		version = "*", -- (recommended) only required with prebuilt binaries
+		dependencies = "saghen/blink.lib",
 
-		-- download prebuilt binaries from github releases
-		dependencies = "saghen/blink.download",
-		-- OR build from source, requires nightly:
-		-- https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-		-- build = 'cargo build --release',
-		-- If you use nix, you can build from source using latest nightly rust with:
-		-- build = 'nix run .#build-plugin',
+		version = "*",
+		-- download prebuilt binaries from github releases, must be on a versioned release
+		---@diagnostic disable-next-line: undefined-field
+		build = function()
+			require("blink.pairs").download():pwait(60000)
+		end,
+		-- OR build from source
+		-- build = function() require('blink.pairs').build():pwait(60000) end,
 
 		--- @module 'blink.pairs'
 		--- @type blink.pairs.Config
@@ -48,31 +49,37 @@ return {
 				-- or disable with `vim.g.pairs = false` (global) and `vim.b.pairs = false` (per-buffer)
 				-- and/or with `vim.g.blink_pairs = false` and `vim.b.blink_pairs = false`
 				disabled_filetypes = {},
+				wrap = {
+					-- move closing pair via motion
+					["<C-b>"] = "motion",
+					-- move opening pair via motion
+					["<C-S-b>"] = "motion_reverse",
+					-- set to 'treesitter' or 'treesitter_reverse' to use treesitter instead of motions
+					-- set to nil, '' or false to disable the mapping
+					-- normal_mode = {} <- for normal mode mappings, only supports 'motion' and 'motion_reverse'
+				},
 				-- see the defaults:
-				-- https://github.com/Saghen/blink.pairs/blob/main/lua/blink/pairs/config/mappings.lua#L14
+				-- https://github.com/Saghen/blink.pairs/blob/main/lua/blink/pairs/config/mappings.lua#L52
 				pairs = {},
 			},
 			highlights = {
-				enabled = false,
-				-- requires require('vim._extui').enable({}), otherwise has no effect
-				-- cmdline = true,
-				-- groups = {
-				--   'BlinkPairsOrange',
-				--   'BlinkPairsPurple',
-				--   'BlinkPairsBlue',
-				-- },
-				-- unmatched_group = 'BlinkPairsUnmatched',
-				--
-				-- -- highlights matching pairs under the cursor
-				-- matchparen = {
-				--   enabled = true,
-				--   -- known issue where typing won't update matchparen highlight, disabled by default
-				--   cmdline = false,
-				--   -- also include pairs not on top of the cursor, but surrounding the cursor
-				--   include_surrounding = false,
-				--   group = 'BlinkPairsMatchParen',
-				--   priority = 250,
-				-- },
+				enabled = true,
+				-- requires require('vim._core.ui2').enable({}), otherwise has no effect
+				cmdline = true,
+				-- set to { 'BlinkPairs' } to disable rainbow highlighting
+				groups = { "BlinkPairsOrange", "BlinkPairsPurple", "BlinkPairsBlue" },
+				unmatched_group = "BlinkPairsUnmatched",
+
+				-- highlights matching pairs under the cursor
+				matchparen = {
+					enabled = true,
+					-- known issue where typing won't update matchparen highlight, disabled by default
+					cmdline = false,
+					-- also include pairs not on top of the cursor, but surrounding the cursor
+					include_surrounding = false,
+					group = "BlinkPairsMatchParen",
+					priority = 250,
+				},
 			},
 			debug = false,
 		},
