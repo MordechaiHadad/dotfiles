cbe22819b25f1162a90442152cd11da3d1b85a25
MordechaiHadad <mordechai.hadad01@gmail.com>
2026-09-22 16:31:30 +0300

    fix: telescope cursor moving back

--- nvim/lua/plugins/heirline.lua
+++ nvim/lua/plugins/heirline.lua
@@ -32,13 +32,6 @@ return {
 		local ModeComponent = {
 			init = function(self)
 				self.mode = vim.fn.mode(1) -- :h mode()
-
-				-- execute this only once, this is required if you want the ViMode
-				-- component to be updated on operator pending mode
-				if not self.once then
-					vim.api.nvim_create_autocmd("ModeChanged", { command = "redrawstatus" })
-					self.once = true
-				end
 			end,
 			-- Now we define some dictionaries to map the output of mode() to the
 			-- corresponding string and color. We can put these into `static` to compute
